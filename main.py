d192e795bc508d82fdeded3b50ba7128f00a5abb
Valery <pro6645@yandex.ru>
2023-06-20 22:15:32 +0300

    get_expend

--- main.py
+++ main.py
@@ -91,17 +91,6 @@ def new_expend(id, date, category, price, user_id):
     # print(form.errors)
 
 
-@app.route('/get_user/<int:id>')
-def get_user(id):
-    db_sess = db_session.create_session()
-    user = db_sess.query(User).filter(User.id == id).first()
-    id = f'"id":"{str(user.id)}",'
-    name = f'"name":"{user.name}",'
-    about = f'"about":"{user.about}",'
-    rate = f'"rate":"{str(user.rate)}"'
-    return jsonify("{" + id + name + about + rate + "}")
-
-
 @app.route('/get_username/<int:id>')
 def get_username(id):
     db_sess = db_session.create_session()
@@ -109,119 +98,17 @@ def get_username(id):
     return user.name
 
 
-def get_event(id):
-    db_sess = db_session.create_session()
-    event = db_sess.query(Events).filter(Events.id == id).first()
-    id = {"id": int(event.id)}
-    title = {"title":f"{event.title}"}
-    content = {"content":f"{event.content}"}
-    date = {"date":f"{event.date}"}
-    time = {"time":f"{event.time}"}
-    place = {"place": start_coordinates(event.place)}
-    category = {"category": int(event.category)}
-    is_private = {"is_private": event.is_private}
-    user_id = {"user_id": int(event.userid)}
-    return id | title | content | date | time | place | category | is_private | user_id
-
-
-@app.route("/new_comment/<int:event_id>/<int:user_id>/<string:content>",
+@app.route("/get_expend/<int:id>",
            methods=['GET', 'POST'])
-def new_comment(event_id, user_id, content):
+def get_expend(id):
     db_sess = db_session.create_session()
-
-    comment = Comment()
-    comment.event_id = event_id
-    comment.user_id = user_id
-    comment.content = content
-
-    db_sess.add(comment)
-    db_sess.commit()
-    return jsonify('true')
-
-
-@app.route('/get_comments/<int:id>')
-def get_comments(id):
-    db_sess = db_session.create_session()
-    comments = db_sess.query(Comment).filter(Comment.event_id == id).all()
-    response = [[get_username(comment.user_id), comment.content] for comment in comments]
-    return jsonify(str(response))
-
-
-def start_coordinates(toponym):
-    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
-
-    geocoder_params = {
-        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-        "geocode": toponym,
-        "format": "json"}
-
-    res = requests.get(geocoder_api_server, params=geocoder_params)
-
-    if not res:
-        print("Ошибка выполнения запроса:")
-        print(res)
-        print("Http статус:", res.status_code, "(", res.reason, ")")
-
-    # Преобразуем ответ в json-объект
-    json_response = res.json()
-    # Получаем первый топоним из ответа геокодера
-    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-    # Координаты центра топонима:
-    toponym_coodrinates = toponym["Point"]["pos"]
-    # Долгота и широта:
-    print(toponym_coodrinates.split(" "))
-    return [float(i) for i in toponym_coodrinates.split(" ")]
-
-
-def param_func(x, y, z, map_type):
-    # Собираем параметры для запроса к StaticMapsAPI:
-    return {
-        "ll": ",".join([x, y]),
-        "z": f'{z}',
-        "l": {map_type},
-        "size": "650,450"
-    }
-
-@app.route('/get_events')
-def get_events():
-    db_sess = db_session.create_session()
-    events = db_sess.query(Events).all()
-    events_ids = [event.id for event in events]
-    response = [get_event(i) for i in events_ids]
-    return jsonify(str(response))
-
-
-@app.route('/booking_event/<int:event_id>/<int:user_id>')
-def booking_event(event_id, user_id):
-    db_sess = db_session.create_session()
-    user = db_sess.query(User).filter(User.id == user_id).first()
-    user.booking = str(user.booking)
-    event_ids = [str(i) for i in user.booking if is_integer(i)]
-    event_ids.append(str(event_id))
-    user.booking = '[' + ', '.join(event_ids) + ']'
-    db_sess.commit()
-    return jsonify('true')
-
-
-@app.route('/unbooking_event/<int:event_id>/<int:user_id>')
-def unbooking_event(event_id, user_id):
-    db_sess = db_session.create_session()
-    user = db_sess.query(User).filter(User.id == user_id).first()
-    user.booking = str(user.booking)
-    event_ids = [str(i) for i in user.booking if is_integer(i) and str(i) != str(event_id)]
-    user.booking = '[' + ', '.join(event_ids) + ']'
-    db_sess.commit()
-    return jsonify('true')
-
-
-@cross_origin()
-@app.route('/get_booking_event/<int:user_id>')
-def get_booking_event(user_id):
-    db_sess = db_session.create_session()
-    user = db_sess.query(User).filter(User.id == user_id).first()
-    events = [get_event(i) for i in user.booking if is_integer(i)]
-    print(events)
-    return jsonify(str(events))
+    expend = db_sess.query(Expend).filter(Expend.id == id).first()
+    id = {"id": int(expend.id)}
+    date = {"date":f"{expend.date}"}
+    category = {"category": int(expend.category)}
+    price = {"price": int(expend.price)}
+    user_id = {"user_id": int(expend.user_id)}
+    return id | date | category | price | user_id
 
 
 def main():
